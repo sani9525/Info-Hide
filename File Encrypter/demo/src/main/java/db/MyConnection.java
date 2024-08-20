8b75b840f157dc04122fe1c0ff15c062c87effdf
Sani Kumar Sharma <82691077+sani9525@users.noreply.github.com>
2024-08-20 19:51:29 +0530

    Add files via upload

---
+++ File Encrypter/demo/src/main/java/db/MyConnection.java
@@ -0,0 +1,30 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyConnection {
+    public static Connection connection = null;
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/file_encrypter?useSSL=false &allowPublicKeyRetrieval=true", "root", "Sani@123");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        // System.out.println("Connection ho gya saab");
+        return connection;
+    }
+    public static void closeConnection() {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+    }
+
+}
